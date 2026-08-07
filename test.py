192265b0d7735d50fc838a96ae30ee8ecd8f9fa8
jerry <jksiongy@gmail.com>
2026-08-07 18:54:06 +0300

    added forms to the login and register pages

---
+++ test.py
@@ -0,0 +1,13 @@
+from rembg import remove
+from PIL import Image
+
+input_path = "static/logo.jpeg"
+output_path ="static/logo2.png"
+
+with open(input_path, 'rb') as inp:
+    input_data = inp.read()
+
+output_data = remove(input_data)
+
+with open(output_path, 'wb') as out:
+    out.write(output_data)
